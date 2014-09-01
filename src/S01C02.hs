f105799b312a4c7b6f1cf0e9efdcf90a77b2f0b7
Devon Stewart <blast@hardchee.se>
2014-09-01 14:37:36 -0700

    S01C02: xor ByteStrings

--- src/S01C02.hs
+++ src/S01C02.hs
@@ -1,4 +1,10 @@
 module S01C02 where
 
+import Data.Bits (xor)
+import qualified Data.ByteString as BS
+
+xorByteStrings :: BS.ByteString -> BS.ByteString -> BS.ByteString
+xorByteStrings a b = BS.pack $ fmap (uncurry xor) $ zip (BS.unpack a) (BS.unpack b)
+
 xorHexStrings :: String -> String -> String
 xorHexStrings _ _ = ""
--- test/S01C02Spec.hs
+++ test/S01C02Spec.hs
@@ -4,12 +4,20 @@ import Test.Hspec
 
 import S01C02
 
+import qualified Data.ByteString as BS
+
 main :: IO()
 main = hspec spec
 
 spec :: Spec
 spec = do
   describe "xor" $ do
+    it "should xor ByteStrings" $ do
+      let cs1 = BS.pack [254, 1]
+      let cs2 = BS.pack [1, 254]
+      let target = BS.pack [255, 255]
+      xorByteStrings cs1 cs2 `shouldBe` target
+
     it "should xor hex strings" $ do
       let cs1 = "1c0111001f010100061a024b53535009181c"
       let cs2 = "686974207468652062756c6c277320657965"
