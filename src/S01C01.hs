4c2534ad95f0f7e9ec4dd0cc5df499a35a453a5e
Devon Stewart <blast@hardchee.se>
2014-09-01 15:02:40 -0700

    S01C01: Simplifying type conversions, all of these are Integrals

--- src/S01C01.hs
+++ src/S01C01.hs
@@ -12,13 +12,13 @@ import qualified Data.Char as C
 -- utilities
 
 w8toInt :: W.Word8 -> Int
-w8toInt = fromIntegral . toInteger
+w8toInt = fromIntegral
 
 w8tow16 :: W.Word8 -> W.Word16
-w8tow16 = fromIntegral . toInteger
+w8tow16 = fromIntegral
 
 w16tow8 :: W.Word16 -> W.Word8
-w16tow8 = fromIntegral . toInteger
+w16tow8 = fromIntegral
 
 pairs :: String -> [(Char, Char)]  -- Drops incomplete pairs. "abc" == [('a', 'b')]
 pairs (c1 : c2 : s) = (c1, c2) : pairs s
