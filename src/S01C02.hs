c275d88d3709e5e717621eb77fdcbf719fbe6885
Devon Stewart <blast@hardchee.se>
2014-09-01 15:00:28 -0700

    S01C02: Reducing complexity

--- src/S01C02.hs
+++ src/S01C02.hs
@@ -6,7 +6,7 @@ import qualified Data.ByteString as BS
 import S01C01 (hexToByteString, bytestringToHex)
 
 xorByteStrings :: BS.ByteString -> BS.ByteString -> BS.ByteString
-xorByteStrings a b = BS.pack $ fmap (uncurry xor) $ zip (BS.unpack a) (BS.unpack b)
+xorByteStrings a b = BS.pack $ BS.zipWith xor a b
 
 xorHexStrings :: String -> String -> String
 xorHexStrings a b = bytestringToHex $ xorByteStrings bsa bsb
