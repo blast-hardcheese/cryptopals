508bab09e443ab3ccfa92c5853cf1705bea8a853
Devon Stewart <blast@hardchee.se>
2014-08-31 20:54:04 -0700

    S01C01: Decode hex string to ByteString

--- src/S01C01.hs
+++ src/S01C01.hs
@@ -1,4 +1,25 @@
 module S01C01 where
 
+import qualified Data.ByteString as BS
+import qualified Data.List as L
+import Data.Maybe
+
+hexToByteString :: String -> BS.ByteString
+hexToByteString = BS.pack . (fmap fromIntegral) . unpackHexPairList . pairs
+
+unpackHexPairList :: [(Char, Char)] -> [Int]
+unpackHexPairList = catMaybes . (fmap unpackHexPair)
+
+unpackHexPair :: (Char, Char) -> Maybe Int
+unpackHexPair (c1, c2) = (unhex c1) >>= (\x -> (unhex c2) >>= (\y -> Just $ x * 16 + y))
+
+unhex :: Char -> Maybe Int
+unhex c = L.findIndex (== c) ord
+         where ord = "0123456789abcdef"
+
+pairs :: String -> [(Char, Char)]  -- Drops incomplete pairs. "abc" == [('a', 'b')]
+pairs (c1 : c2 : s) = (c1, c2) : pairs s
+pairs _ = []
+
 base64 :: String -> Maybe String
 base64 _ = Nothing
--- test/S01C01Spec.hs
+++ test/S01C01Spec.hs
@@ -4,11 +4,25 @@ import Test.Hspec
 
 import S01C01
 
+import qualified Data.ByteString as BS
+
 main :: IO()
 main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "base64 helpers" $ do
+    it "should decode a base64 Char" $ do
+        unhex 'a' `shouldBe` Just 10
+
+    it "should decode a base64 pair" $ do
+        unpackHexPair ('1', '3') `shouldBe` Just 19
+
+    it "should decode a list of base64 pairs" $ do
+        unpackHexPairList [('1', '3'), ('a', '1')] `shouldBe` [19, 161]
+
+    it "should decode a base64 string" $ do
+        hexToByteString "13a1" `shouldBe` BS.pack [19, 161]
 
   describe "base64" $ do
     it "should encode a hex string into base64" $ do
