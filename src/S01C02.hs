d43b1a893a9b38de3c9b4902829c6ada45878175
Devon Stewart <blast@hardchee.se>
2014-09-01 14:37:52 -0700

    S01C02: xor hex strings

--- src/S01C02.hs
+++ src/S01C02.hs
@@ -3,8 +3,12 @@ module S01C02 where
 import Data.Bits (xor)
 import qualified Data.ByteString as BS
 
+import S01C01 (hexToByteString, bytestringToHex)
+
 xorByteStrings :: BS.ByteString -> BS.ByteString -> BS.ByteString
 xorByteStrings a b = BS.pack $ fmap (uncurry xor) $ zip (BS.unpack a) (BS.unpack b)
 
 xorHexStrings :: String -> String -> String
-xorHexStrings _ _ = ""
+xorHexStrings a b = bytestringToHex $ xorByteStrings bsa bsb
+    where bsa = hexToByteString a
+          bsb = hexToByteString b
