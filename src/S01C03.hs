a15b65270214184262e65d1f133a72dcf58db89f
Devon Stewart <blast@hardchee.se>
2014-09-01 23:03:47 -0700

    S01C03: Build frequency letter maps from ByteString

--- src/S01C03.hs
+++ src/S01C03.hs
@@ -1,11 +1,44 @@
+{-# LANGUAGE PackageImports #-}
 module S01C03 where
 
 import Data.Bits
+import qualified Data.HashMap as HM
+import qualified "hashmap" Data.HashSet as HS
 import qualified Data.Word as W
 import qualified Data.ByteString as BS
+import qualified Data.Char as C
 
+import qualified S01C02
+
+-- Utility functions
+c2w8 :: Char -> W.Word8
+c2w8 = fromIntegral . C.ord
+
+w8toChar :: W.Word8 -> Char
+w8toChar = C.chr . fromIntegral
+
+s2ByteString :: String -> BS.ByteString
+s2ByteString = BS.pack . (fmap c2w8)
+
+-- Apply single-byte xor
 applySingleByte :: W.Word8 -> BS.ByteString -> BS.ByteString
 applySingleByte b bs = BS.map (xor b) bs
+
+-- Frequency analysis
+buildLetterMap :: BS.ByteString -> HM.Map Char Int
+buildLetterMap bs = BS.foldr (updateMap . w8toChar) HM.empty bs
+    where updateMap = \k m -> HM.insertWith (\_ v -> v + 1) (C.toLower k) 1 m
+
+filterLetters :: HM.Map Char Int -> HM.Map Char Int
+filterLetters = HM.filterWithKey isLetterKV
+    where letterSet = HS.fromList "abcdefghijklmnopqrstuvwxyz"
+          isLetter = (flip HS.member) letterSet
+          isLetterKV = \k _ -> isLetter k
+
+buildFilteredLetterMap :: BS.ByteString -> HM.Map Char Int
+buildFilteredLetterMap = filterLetters . buildLetterMap
+
+-- Process xor stuff
 
 analyze :: String -> W.Word8
 analyze _ = 0
--- test/S01C03Spec.hs
+++ test/S01C03Spec.hs
@@ -2,6 +2,8 @@ module S01C03Spec (main, spec) where
 
 import Test.Hspec
 import qualified Data.ByteString as BS
+import qualified Data.HashMap as HM
+import qualified Data.List as L
 
 import S01C03
 
@@ -17,6 +19,16 @@ spec = do
 
       applySingleByte 0 bs `shouldBe` bs
       applySingleByte 1 bs `shouldBe` target
+
+    it "should build letter maps from input" $ do
+      let letters = s2ByteString "aabccc"
+      buildLetterMap letters `shouldBe` HM.fromList [('a', 2), ('b', 1), ('c', 3)]
+
+    it "should strip non-letter characters" $ do
+      let hm = buildFilteredLetterMap $ s2ByteString "This is a test."
+      let letters = "thisae"
+
+      (L.sort $ HM.keys hm) `shouldBe` (L.sort letters)
 
     it "should find cypher byte" $ do
       let ct = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
