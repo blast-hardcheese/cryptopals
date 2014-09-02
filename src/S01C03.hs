5061cbd0e8166e542a8a9a76816c602865c133ae
Devon Stewart <blast@hardchee.se>
2014-09-01 20:55:21 -0700

    S01C03: xor a single byte against a ByteString

--- src/S01C03.hs
+++ src/S01C03.hs
@@ -1,6 +1,11 @@
 module S01C03 where
 
+import Data.Bits
 import qualified Data.Word as W
+import qualified Data.ByteString as BS
+
+applySingleByte :: W.Word8 -> BS.ByteString -> BS.ByteString
+applySingleByte b bs = BS.map (xor b) bs
 
 analyze :: String -> W.Word8
 analyze _ = 0
--- test/S01C03Spec.hs
+++ test/S01C03Spec.hs
@@ -1,6 +1,7 @@
 module S01C03Spec (main, spec) where
 
 import Test.Hspec
+import qualified Data.ByteString as BS
 
 import S01C03
 
@@ -10,6 +11,13 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "single-byte xor" $ do
+    it "should apply single byte to ByteString" $ do
+      let bs = BS.pack [1,2,3,4,5,6]
+      let target = BS.pack [0, 3, 2, 5, 4, 7]
+
+      applySingleByte 0 bs `shouldBe` bs
+      applySingleByte 1 bs `shouldBe` target
+
     it "should find cypher byte" $ do
       let ct = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
       analyze ct `shouldBe` 5
